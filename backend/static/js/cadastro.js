0932bdf3d2f440c855b36e742e6b10889f6932f0
Vinicius <vinicius_antunes7@hotmail.com>
2022-03-17 17:46:21 -0300

    cadastro backend

--- backend/static/js/cadastro.js
+++ backend/static/js/cadastro.js
@@ -1,3 +1,19 @@
+document.querySelector("form").addEventListener("submit", async (e) => {
+    e.preventDefault()
+    console.log(e)
+    return
+    const response = await fetch('/user/signup', {method: 'post'})
+    const json = await res.json()
+})
+
+
+
+const user = json;
+console.log(json)
+localStorage.setItem("crypto.edu.user", JSON.stringify(user))
+const localUser = JSON.parse(localStorage.getItem("crypto.edu.user"))
+
+
 function validar() {
     var email = formuser.email.value;
     var usuario = formuser.usuario.value;
