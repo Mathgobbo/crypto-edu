381ecb4c4ee599a9572b6650fc52050452242fec
Matheus Gobbo Bernardi <matgobbo@hotmail.com>
2022-03-17 17:18:50 -0300

    Conexão com o Banco e Schemas

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,20 @@ const app = express();
 const cors = require("cors");
 
 const router = require("./src/routes");
+const { connectDatabase } = require("./src/services/mongodb");
 app.use(cors());
 app.use(router);
 
 app.use(express.static("static"));
 
-app.listen(8080, () => console.log("Listening on 8080"));
+async function main() {
+  try {
+    await connectDatabase();
+    app.listen(8080, () => console.log("Listening on 8080"));
+  } catch (error) {
+    console.log(error);
+    process.exit(0);
+  }
+}
+
+main();
